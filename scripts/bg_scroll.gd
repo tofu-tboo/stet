7eba43219c14512fe92cb074bfe59a588284d0ba
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2026-01-09 04:03:17 +0900

    0109 bg art, scrolling, responsive window size

---
+++ scripts/bg_scroll.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var sprite: Sprite2D = $Sprite
+var speed: float = 100.0
+
+func _ready() -> void:
+	set_process(false)
+
+func _physics_process(delta: float) -> void:
+	sprite.region_rect.position.y += delta * speed
+	
+	if sprite.region_rect.position.y >= 2 ** 16:
+		sprite.region_rect.position.y -= 2 ** 16
---
+++ scripts/global/responsive_window.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func _ready():
+	if OS.has_feature("editor") or OS.has_feature("web"):
+		return
+		
+	var base: Vector2i = Vector2i(ProjectSettings.get_setting_with_override(&"display/window/size/viewport_width"), ProjectSettings.get_setting_with_override(&"display/window/size/viewport_height"))
+	var screen_size: Vector2i = DisplayServer.screen_get_size()
+
+	var scale_x: int = screen_size.x / base.x
+	var scale_y: int = screen_size.y / base.y
+	var scale: float = min(scale_x, scale_y)
+
+	# 최소 1배 보장
+	scale = max(scale, 1)
+
+	var window_size: Vector2i = base * (scale-1)
+	DisplayServer.window_set_size(window_size)
+
+	# 중앙 정렬
+	var pos: Vector2i = (screen_size - window_size) / 2
+	DisplayServer.window_set_position(pos)
